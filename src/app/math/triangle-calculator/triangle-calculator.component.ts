dd7118a5b0112eabc4666d4ce84f8253be0ed401
kevin stamenkovski <kevin.stamenkovski@gmail.com>
2022-06-15 11:02:52 +0200

    added triangle image to the triangle-calculator

--- src/app/math/triangle-calculator/triangle-calculator.component.ts
+++ src/app/math/triangle-calculator/triangle-calculator.component.ts
@@ -13,17 +13,17 @@ export class TriangleCalculatorComponent implements OnInit {
   HeightFormControl: FormControl;
   perimeter: number = 0;
   area: number = 0;
+  public addDisplay: string = '';
   constructor() {
     this.BaseFormControl = new FormControl(0);
     this.SecondSideFormControl = new FormControl(0);
     this.ThirdSideFormControl = new FormControl(0);
-    this.HeightFormControl = new FormControl('');
+    this.HeightFormControl = new FormControl(0);
   }
   public calculate(): void{
     this.perimeter = parseFloat(this.BaseFormControl.value) + parseFloat(this.SecondSideFormControl.value) + parseFloat(this.ThirdSideFormControl.value);
     this.area = parseFloat(this.BaseFormControl.value) * parseFloat(this.HeightFormControl.value) / 2;
+    this.addDisplay = 'display: block;';
   }
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void {}
 }
