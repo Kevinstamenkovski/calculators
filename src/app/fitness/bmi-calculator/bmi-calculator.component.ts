bdc6d80d6aebffd1156cbb40a8a13841ceb3b5ad
kevin stamenkovski <kevin.stamenkovski@gmail.com>
2022-06-16 19:54:03 +0200

    MBI calculator

--- src/app/fitness/bmi-calculator/bmi-calculator.component.ts
+++ src/app/fitness/bmi-calculator/bmi-calculator.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl} from "@angular/forms";
 
 @Component({
   selector: 'app-bmi-calculator',
@@ -6,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bmi-calculator.component.scss']
 })
 export class BMICalculatorComponent implements OnInit {
-
-  constructor() { }
-
+  WeightFormControl: FormControl;
+  HeightFormControl: FormControl;
+  AgeFormControl: FormControl;
+  public result: number = 0;
+  public output: string = '';
+  constructor() {
+    this.WeightFormControl = new FormControl(0);
+    this.HeightFormControl = new FormControl(0);
+    this.AgeFormControl = new FormControl( 0);
+  }
+  public calculate(): void{
+    this.result = parseFloat(this.WeightFormControl.value) / (Math.pow(parseFloat(this.HeightFormControl.value), 2) /100);
+    if(this.result < 16){
+      this.output = this.result + " Severe Thinness";
+    }else if(this.result < 17){
+      this.output = this.result + " Moderate Thinness"
+    }else if(this.result < 18.5){
+      this.output = this.result + " Mild Thinness"
+    }else if(this.result < 25){
+      this.output = this.result + " Normal"
+    }else if(this.result < 30){
+      this.output = this.result + " Overweight"
+    }else{
+      this.output = this.result + " Obese"
+    }
+  }
   ngOnInit(): void {
   }
 
