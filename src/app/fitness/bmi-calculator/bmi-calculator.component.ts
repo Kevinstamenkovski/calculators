a3a9a6562d4fc51d4d01c9d6d05ece51ba873c24
kevin stamenkovski <kevin.stamenkovski@gmail.com>
2022-06-16 13:26:33 +0200

    BMI calculator

---
+++ src/app/fitness/bmi-calculator/bmi-calculator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bmi-calculator',
+  templateUrl: './bmi-calculator.component.html',
+  styleUrls: ['./bmi-calculator.component.scss']
+})
+export class BMICalculatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
