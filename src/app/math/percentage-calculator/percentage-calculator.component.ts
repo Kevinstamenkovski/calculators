14abc221649310c0d017176e80760325828c712a
kevin stamenkovski <kevin.stamenkovski@gmail.com>
2022-06-16 10:27:45 +0200

    modified percentage calculator

--- src/app/math/percentage-calculator/percentage-calculator.component.ts
+++ src/app/math/percentage-calculator/percentage-calculator.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl} from "@angular/forms";
 
 @Component({
   selector: 'app-percentage-calculator',
@@ -6,8 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./percentage-calculator.component.scss']
 })
 export class PercentageCalculatorComponent implements OnInit {
-
-  constructor() { }
+  PercentFormControl: FormControl;
+  IntNumberFormControl: FormControl;
+  public addDisplay: string = 'display: none;';
+  result: number = 0;
+  constructor() {
+    this.PercentFormControl = new FormControl('');
+    this.IntNumberFormControl = new FormControl('');
+  }
+  public calculate(): void{
+    this.result = parseFloat(this.IntNumberFormControl.value) / 100 * parseFloat(this.PercentFormControl.value);
+    this.addDisplay = 'display: block;'
+  }
 
   ngOnInit(): void {
   }
