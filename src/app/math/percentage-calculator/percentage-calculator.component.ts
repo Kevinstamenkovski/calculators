1dab687f9f34a28ed02467c78bed0a1a3b0ac1e3
kevin stamenkovski <kevin.stamenkovski@gmail.com>
2022-06-16 09:05:17 +0200

    percentage calculator component

---
+++ src/app/math/percentage-calculator/percentage-calculator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-percentage-calculator',
+  templateUrl: './percentage-calculator.component.html',
+  styleUrls: ['./percentage-calculator.component.scss']
+})
+export class PercentageCalculatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
