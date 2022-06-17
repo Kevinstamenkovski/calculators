5c694e814b4dc02b7e44a77b805bb690841da560
kevin stamenkovski <kevin.stamenkovski@gmail.com>
2022-06-17 12:53:03 +0200

    ideal weight calculator

--- src/app/fitness/ideal-weight-calculator/ideal-weight-calculator.component.ts
+++ src/app/fitness/ideal-weight-calculator/ideal-weight-calculator.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl} from "@angular/forms";
 
 @Component({
   selector: 'app-ideal-weight-calculator',
@@ -6,8 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ideal-weight-calculator.component.scss']
 })
 export class IdealWeightCalculatorComponent implements OnInit {
-
-  constructor() { }
+  HeightFormControl: FormControl;
+  AgeFormControl: FormControl;
+  public result: number = 0;
+  public output: string = '';
+  public maleS: boolean = true;
+  public femaleS: boolean = false;
+  constructor() {
+    this.HeightFormControl = new FormControl(0);
+    this.AgeFormControl = new FormControl( 0);
+  }
+  public maleSelected(): void{
+    this.maleS = true;
+    this.femaleS = false;
+  }
+  public femaleSelected(): void{
+    this.maleS = false;
+    this.femaleS = true;
+  }
+  public calculate(): void{
+    if (this.maleS){
+      this.result = (50 + (.91 * (this.HeightFormControl.value - 152.4)));
+    }else if(this.femaleS){
+      this.result = (45.5 + (.91 * (this.HeightFormControl.value - 152.4)));
+    }
+    this.output = this.result.toFixed(1) + " Kg";
+  }
 
   ngOnInit(): void {
   }
