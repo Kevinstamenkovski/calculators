6587fe029b2120d24b0e9a60c65d7d692daff7a2
kevin stamenkovski <kevin.stamenkovski@gmail.com>
2022-06-17 12:19:58 +0200

    BMR completed with no scss

--- src/app/fitness/bmr-calculator/bmr-calculator.component.ts
+++ src/app/fitness/bmr-calculator/bmr-calculator.component.ts
@@ -10,23 +10,32 @@ export class BMRCalculatorComponent implements OnInit {
   WeightFormControl: FormControl;
   HeightFormControl: FormControl;
   AgeFormControl: FormControl;
-  femaleSelected: FormControl;
-  maleSelected: FormControl;
   public result: number = 0;
   public output: string = '';
+  public maleS: boolean = true;
+  public femaleS: boolean = false;
   constructor() {
     this.WeightFormControl = new FormControl(0);
     this.HeightFormControl = new FormControl(0);
     this.AgeFormControl = new FormControl( 0);
-    this.femaleSelected = new FormControl('');
-    this.maleSelected = new FormControl('');
+  }
+  public maleSelected(): void{
+    this.maleS = true;
+    this.femaleS = false;
+  }
+  public femaleSelected(): void{
+    this.maleS = false;
+    this.femaleS = true;
   }
   public calculate(): void{
-    if(this.maleSelected){
-      alert("GAY")
+    if (this.maleS){
+      this.result = (10 * this.WeightFormControl.value) + (6.25 * this.HeightFormControl.value) - (5 * this.AgeFormControl.value + 5);
+    }else if(this.femaleS){
+      this.result = (10 * this.WeightFormControl.value) + (6.25 * this.HeightFormControl.value) - (5 * this.AgeFormControl.value - 161);
     }
-
+    this.output = this.result + " Calories/Day";
   }
+
   ngOnInit(): void {
   }
 
