f4bcba575a2580873d0431714a980cd9215b25d4
kevin stamenkovski <kevin.stamenkovski@gmail.com>
2022-06-23 14:03:05 +0200

    fixed algorithm if there isn't a value

--- src/app/fitness/bmi-calculator/bmi-calculator.component.ts
+++ src/app/fitness/bmi-calculator/bmi-calculator.component.ts
@@ -8,31 +8,36 @@ import { FormControl } from "@angular/forms";
   styleUrls: ['./bmi-calculator.component.scss']
 })
 export class BMICalculatorComponent implements OnInit {
-  WeightFormControl: FormControl;
-  HeightFormControl: FormControl;
-  AgeFormControl: FormControl;
-  public result: number = 0;
-  public output: string = '';
+  WeightFormControl: FormControl
+  HeightFormControl: FormControl
+  AgeFormControl: FormControl
+  public result: number = 0
+  public output: string = ''
   constructor() {
-    this.WeightFormControl = new FormControl(0);
-    this.HeightFormControl = new FormControl(0);
-    this.AgeFormControl = new FormControl( 0);
+    this.WeightFormControl = new FormControl(0)
+    this.HeightFormControl = new FormControl(0)
+    this.AgeFormControl = new FormControl( 0)
   }
   public calculate(): void{
-    this.result = parseFloat(this.WeightFormControl.value) / (Math.pow(parseFloat(this.HeightFormControl.value), 2) /10000);
-    if(this.result < 16){
-      this.output = this.result + " Severe Thinness";
-    }else if(this.result < 17){
-      this.output = this.result + " Moderate Thinness"
-    }else if(this.result < 18.5){
-      this.output = this.result + " Mild Thinness"
-    }else if(this.result < 25){
-      this.output = this.result + " Normal"
-    }else if(this.result < 30){
-      this.output = this.result + " Overweight"
+    if(this.WeightFormControl.value == 0 || this.HeightFormControl.value == 0 || this.WeightFormControl.value == null || this.HeightFormControl.value == null){
+      alert("assaf")
     }else{
-      this.output = this.result + " Obese"
+      this.result = (parseFloat(this.WeightFormControl.value) / (Math.pow(parseFloat(this.HeightFormControl.value), 2) /10000))
+      if(this.result < 16){
+        this.output = this.result.toFixed(1) + " Severe Thinness"
+      }else if(this.result < 17){
+        this.output = this.result.toFixed(1) + " Moderate Thinness"
+      }else if(this.result < 18.5){
+        this.output = this.result.toFixed(1) + " Mild Thinness"
+      }else if(this.result < 25){
+        this.output = this.result.toFixed(1) + " Normal"
+      }else if(this.result < 30){
+        this.output = this.result.toFixed(1) + " Overweight"
+      }else{
+        this.output = this.result.toFixed(1) + " Obese"
+      }
     }
+
   }
   ngOnInit(): void {
   }
