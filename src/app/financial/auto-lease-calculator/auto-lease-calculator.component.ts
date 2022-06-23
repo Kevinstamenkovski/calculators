b2d5ea0a18cc0cd2fcf920bc279353d3a9433096
kevin stamenkovski <kevin.stamenkovski@gmail.com>
2022-06-23 10:26:19 +0200

    auto lease calculator

---
+++ src/app/financial/auto-lease-calculator/auto-lease-calculator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-auto-lease-calculator',
+  templateUrl: './auto-lease-calculator.component.html',
+  styleUrls: ['./auto-lease-calculator.component.scss']
+})
+export class AutoLeaseCalculatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
