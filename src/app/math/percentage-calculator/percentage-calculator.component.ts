bfd0fa6a120ec3df23271ef02d7ece6d5f21037c
kevin stamenkovski <kevin.stamenkovski@gmail.com>
2022-06-24 10:29:49 +0200

    debug

--- src/app/math/percentage-calculator/percentage-calculator.component.ts
+++ src/app/math/percentage-calculator/percentage-calculator.component.ts
@@ -14,29 +14,40 @@ export class PercentageCalculatorComponent implements OnInit {
   result: number = 0;
   finalResult: string = '';
   constructor() {
-    this.PercentFormControl = new FormControl(0);
-    this.IntNumberFormControl = new FormControl(0);
-    this.ResultFormControl = new FormControl(0);
+    this.PercentFormControl = new FormControl('');
+    this.IntNumberFormControl = new FormControl('');
+    this.ResultFormControl = new FormControl('');
   }
   public calculate(): void{
 
-    if(this.IntNumberFormControl.value != 0 && this.PercentFormControl.value != 0 && this.ResultFormControl.value == 0){
+    if(this.IntNumberFormControl.value != '' && this.PercentFormControl.value != '' && this.ResultFormControl.value == ''){
       this.result = parseFloat(this.IntNumberFormControl.value) / 100 * parseFloat(this.PercentFormControl.value);
       this.finalResult = this.PercentFormControl.value + "% OF " + this.IntNumberFormControl.value + " IS " + this.result;
-    }else if(this.IntNumberFormControl.value != 0 && this.ResultFormControl.value != 0 && this.PercentFormControl.value == 0){
+    }else if(this.IntNumberFormControl.value != '' && this.ResultFormControl.value != '' && this.PercentFormControl.value == ''){
       this.result = parseFloat(this.IntNumberFormControl.value) / parseFloat(this.ResultFormControl.value) * 100;
       this.finalResult = "The % OF " + this.IntNumberFormControl.value + " OF " + this.ResultFormControl.value + " IS " + this.result + "%";
-    }else if(this.ResultFormControl.value != 0 && this.PercentFormControl.value != 0 && this.IntNumberFormControl.value == 0){
+    }else if(this.ResultFormControl.value != '' && this.PercentFormControl.value != '' && this.IntNumberFormControl.value == ''){
       this.result = parseFloat(this.ResultFormControl.value) / parseFloat(this.PercentFormControl.value) * 100;
       this.finalResult = this.ResultFormControl.value + " IS THE " + this.PercentFormControl.value + "% OF " + this.result;
+    }else if(this.IntNumberFormControl.value !== '' && this.PercentFormControl.value !== '' && this.ResultFormControl.value !== ''){
+      alert("Too Many Inputs")
     }else{
-      alert("You Must Input At least 2 Values")
+      alert("You Need At Least 2 Inputs")
     }
 
    this.addDisplay = 'display: block;'
   }
   public reset(): void{
-    window.location.reload()
+    setTimeout(() => {
+      this.PercentFormControl.setValue('')
+    }, 200)
+    setTimeout(() => {
+      this.IntNumberFormControl.setValue('')
+    }, 400)
+    setTimeout(() => {
+      this.ResultFormControl.setValue('')
+    }, 600)
+    this.addDisplay = 'display: none;'
   }
   ngOnInit(): void {
   }
